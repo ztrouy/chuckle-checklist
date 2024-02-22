5669dcf3efba263e46c97afc70194206ad498e4e
Zachary Trouy <ztrouy@gmail.com>
2024-02-22 09:48:07 -0600

    Added input field for typing a new joke

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react"
 import "./App.css"
 
 export const App = () => {
-  return <div>Hello World!</div>
+  const [newJoke, setNewJoke] = useState("")
+  
+  return <div className="joke-add-form"> 
+  <input
+    className="joke-input"
+    type="text"
+    placeholder="New One Liner"
+    onChange={event => {
+      setNewJoke(event.target.value)
+    }}
+  />
+  </div>
 }
